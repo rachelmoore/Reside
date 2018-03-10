a72bf0a8d2a29e214c8ca64b3e05ba5efff84a4b
Rachel Moore <rachel@rachelmoore.net>
2018-03-10 01:11:14 -0800

    setState working within componentDidMount

--- reside-test/src/Properties.js
+++ reside-test/src/Properties.js
@@ -2,10 +2,57 @@ import React, { Component } from 'react';
 import './Properties.css';
 
 class Properties extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {properties: 'false'};
+    }
+
+    
+
+    componentDidMount() {
+        const parent = this;
+        fetch('https://api.simplyrets.com/openhouses', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + btoa('simplyrets:simplyrets')
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            console.log('PROPERTIES:', json);
+            this.setState({properties: 'test string'});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    // getProperties = () => {
+
+    //     fetch('https://api.simplyrets.com/openhouses', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization': 'Basic ' + btoa('simplyrets:simplyrets')
+    //         }
+    //     })
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (jsonProperties) {
+    //             console.log(jsonProperties);
+    //             this.setState({ properties: jsonProperties })
+    //         });
+    // }
+
     render() {
         return (
             <div className="Properties">
                 Hello World!
+                {this.state.properties}
+                
             </div>
         );
     }
