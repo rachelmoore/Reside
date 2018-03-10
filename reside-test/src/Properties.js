dab5be7698c848391d6e6b6ff103df0788e25d4f
Rachel Moore <rachel@rachelmoore.net>
2018-03-09 23:20:42 -0800

    Configure App, make Properties component

---
+++ reside-test/src/Properties.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Properties.css';
+
+class Properties extends Component {
+    render() {
+        return (
+            <div className="Properties">
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default Properties;
