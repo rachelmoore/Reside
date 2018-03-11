ffc1af8dc5706425d9573f3037d36abd9c7d7c2f
Rachel Moore <rachel@rachelmoore.net>
2018-03-10 18:40:02 -0800

    Convert Price and Date functions

--- reside-test/src/Properties.js
+++ reside-test/src/Properties.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './Properties.css';
 
 class Properties extends Component {
+    constructor() {
+        super();
+        this.convertDate = this.convertDate.bind(this);
+        this.convertPrice = this.convertPrice.bind(this);
+    }
+
+    convertDate(date) {
+        let slicedDate = date.slice(0, 10).split('-');
+        return slicedDate[1] + '/' + slicedDate[2] + '/' + slicedDate[0]; // 10/30/2010
+    }
+
+    convertPrice(price) {
+       return price.toLocaleString();
+    }
 
     render() {
         return (
@@ -11,8 +25,8 @@ class Properties extends Component {
                 <p>Full Baths: {this.props.details.listing.property.bathsFull}</p>
                 <p>Half Baths: {this.props.details.listing.property.bathsHalf}</p>
                 <p>MLS ID: {this.props.details.listing.mlsId}</p>
-                <p>List Date: {this.props.details.listing.listDate}</p>
-                <p>List Price: {this.props.details.listPrice}</p>
+                <p>List Date: {this.convertDate(this.props.details.listing.listDate)}</p>
+                <p>List Price: ${this.convertPrice(this.props.details.listing.listPrice)}</p>
             </li>
         );
     }
