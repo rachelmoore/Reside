4c7773ceffd17966f4aee54625ac82f4f1853cba
Rachel Moore <rachel@rachelmoore.net>
2018-03-10 19:07:31 -0800

    Brighten up home images

--- reside-test/src/Properties.js
+++ reside-test/src/Properties.js
@@ -20,16 +20,40 @@ class Properties extends Component {
     render() {
         return (
             <li className="a-listing">
-                <img src={`${this.props.details.listing.photos[0]}`} alt={`MLS ID #${this.props.details.listing.mlsId}`} />
-                <p>Bedrooms: {this.props.details.listing.property.bedrooms}</p>
-                <p>Full Baths: {this.props.details.listing.property.bathsFull}</p>
-                <p>Half Baths: {this.props.details.listing.property.bathsHalf}</p>
-                <p>MLS ID: {this.props.details.listing.mlsId}</p>
-                <p>List Date: {this.convertDate(this.props.details.listing.listDate)}</p>
-                <p>List Price: ${this.convertPrice(this.props.details.listing.listPrice)}</p>
+                <img className="home-img" src={`${this.props.details.listing.photos[0]}`} alt={`MLS ID #${this.props.details.listing.mlsId}`} />
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Bedrooms:</td>
+                            <td>{this.props.details.listing.property.bedrooms}</td>
+                        </tr>
+                        <tr>
+                            <td>Full Baths:</td>
+                            <td>{this.props.details.listing.property.bathsFull}</td>
+                        </tr>
+                        <tr>
+                            <td>Half Baths:</td>
+                            <td>{this.props.details.listing.property.bathsHalf}</td>
+                        </tr>
+                        <tr>
+                            <td>MLS ID:</td>
+                            <td>#{this.props.details.listing.mlsId}</td>
+                        </tr>
+                        <tr>
+                            <td>List Date:</td>
+                            <td>{this.convertDate(this.props.details.listing.listDate)}</td>
+                        </tr>
+                        <tr>
+                            <td>List Price:</td>
+                            <td>${this.convertPrice(this.props.details.listing.listPrice)}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </li>
         );
     }
 }
 
 export default Properties;
+
+
