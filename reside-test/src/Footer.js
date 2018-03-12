7846785edf1ac9ebc5f2717dd40d9a778de86dba
Rachel Moore <rachel@rachelmoore.net>
2018-03-11 20:19:26 -0700

    Button styling

--- reside-test/src/Footer.js
+++ reside-test/src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer-container">
-                <p className="footer-text">Made with 💙 and <img className="react-logo" src="http://res.cloudinary.com/df9oqycdp/image/upload/v1520821051/reactsmallest_ychis9.png" /> by Rachel Moore</p>
+                <p className="footer-text">Made with 💙 and <img className="react-logo" src="http://res.cloudinary.com/df9oqycdp/image/upload/v1520821051/reactsmallest_ychis9.png" alt="React logo" /> by Rachel Moore</p>
             </div>
         );
     }
